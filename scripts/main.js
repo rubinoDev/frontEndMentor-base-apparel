f616749e58c0d83ffe9abb0ba8a0432ca33927b9
Victor Rubiño <105171818+rubinoDev@users.noreply.github.com>
2022-06-14 16:19:24 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,10 +22,6 @@ form.addEventListener('submit', e=>{
 })
 
 
-if (form.classList.contains('error')){
-  console.log('ta pegando')
-}
-
 function validateEmail(email) {
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
